d19ea53c6df7780b75b7747884d5d13f4f16cdea
Gerson Janhuel <gersonjanhuel@gmail.com>
2022-11-03 21:20:47 +0700

    Initial Commit

---
+++ UnitTesting_HandsOn_Starter/UnitTesting_HandsOn_StarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTesting_HandsOn_StarterApp.swift
+//  UnitTesting_HandsOn_Starter
+//
+//  Created by Gerson Janhuel on 03/11/22.
+//
+
+import SwiftUI
+
+@main
+struct UnitTesting_HandsOn_StarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
